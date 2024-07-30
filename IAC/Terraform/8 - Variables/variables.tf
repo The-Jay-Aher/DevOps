3eaa6bbc0c16052ea3880fad5e8e3283517ab468
Jay Aher <jaysureshaher@gmail.com>
2024-07-30 08:55:13 +0530

    Update 7.30.1

---
+++ IAC/Terraform/8 - Variables/variables.tf
@@ -0,0 +1,16 @@
+variable "vpn_id" {
+  default = "101.20.30.50/32"
+}
+
+variable "app_port" {
+  default = "8080"
+}
+
+variable "ssh_port" {
+  default = "22"
+}
+
+variable "ftp_port" {
+  default = "20"
+}
+
