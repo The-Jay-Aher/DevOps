bf25181c7cfb7fc23f3621d59e2cb8764f4cf943
Jay-Aher-Vivo-15 <jaysureshaher@gmail.com>
2024-09-10 20:26:34 +0530

    Update 09.10.1

--- IAC/Terraform/01 - Deploying Infrastructure in Terraform/01 - First Server/main.tf
+++ IAC/Terraform/01 - Deploying Infrastructure in Terraform/01 - First Server/main.tf
@@ -12,10 +12,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "Terraform-Server" {
-  ami           = "ami-068e0f1a600cd311c"
-  instance_type = "t2.micro"
-
+  ami                    = "ami-068e0f1a600cd311c"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.Security-Group-1]
   tags = {
     Name = "First-Terraform-Server"
+  }
+
+  user_data = <<-EOF
+      #!/bin/bash
+      echo "Hello, World!" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+
+}
+
+resource "aws_security_group" "Security-Group-1" {
+  name = "Terraform Server Security Group"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
